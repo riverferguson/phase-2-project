1339d9f5d6bdc0ede11d660e03f7a7c1fdbf5491
Ryan <salvat36@gmail.com>
2023-05-13 01:27:12 -0500

    add Nav Component and blank Nav CSS

---
+++ phase-2-project/src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+        <a href="/" className="Guitar-Heroes">
+       LOGO Guitar Heros
+        </a>
+        <ul>
+            <li>
+                <a href="FOOTER">  Search</a>
+            </li>
+            <li>
+                <a href="FOOTER">  Shop</a>
+            </li>
+            <li>
+            <a href="FOOTER">  Cart</a>
+            </li>
+            <li>
+            <a href="FOOTER">  Sell</a>
+            </li>
+            <li>
+            <a href="FOOTER"> Contact us</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
