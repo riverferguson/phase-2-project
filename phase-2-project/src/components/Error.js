7a61157dfc6d4e839db32bfc83a8201baf595a62
Ryan <salvat36@gmail.com>
2023-05-13 09:02:21 -0500

    add footer with icon styling

--- phase-2-project/src/components/Error.js
+++ phase-2-project/src/components/Error.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
+    <Link to='' >
     <div>404 Page Not Found</div>
+    </Link>
   )
 }
 
--- phase-2-project/src/components/Footer.js
+++ phase-2-project/src/components/Footer.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { AppBar, CssBaseline, Toolbar, Typography} from "@mui/material";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Footer = () => {
   return (
-    <div className="footer-main-container">
-        <h3>Connect With Us</h3>
-        <ul>
-            <li>
-                <Link to=''>YouTube</Link>
-            </li>
-            <li>
-                <Link to=''>Instagram</Link>
-            </li>
-            <li>
-                <Link to=''>FaceBook</Link>
-            </li>
-            <li>
-                <Link to=''>Twitter</Link>
-            </li>
-        </ul>
-    </div>
+        <>
+   <CssBaseline/>
+   <AppBar position="relative">
+    <Toolbar>
+        <Typography variant="h6">
+            <TwitterIcon sx={{mr: 2}} />
+            <FacebookIcon sx={{mr: 2}} />
+            <InstagramIcon sx={{mr: 2}} />
+            <MailOutlineIcon sx={{mr: 2}} />
+            Guitar Trader Inc.
+        </Typography>
+    </Toolbar>
+   </AppBar>
+   </>
   )
 }
 
