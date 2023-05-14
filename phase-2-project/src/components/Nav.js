8d134c2e40b6a879eb3359d0faf13d5ed11af111
Ryan <salvat36@gmail.com>
2023-05-14 01:32:55 -0500

    nav component change to initial structure/style

--- phase-2-project/src/components/Nav.js
+++ phase-2-project/src/components/Nav.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { AppBar, CssBaseline, Toolbar, Typography} from "@mui/material";
 import './Nav.css'
 
 
 const Nav = ({handleDarkModeClick, darkMode}) => {
 
   const buttonContent = darkMode ? "Light Mode" : "Dark Mode"
-
-  return (
-  <>
-<span>
-  <CssBaseline/>
-  <AppBar className='appbar' sx={{bgcolor: "green" }} position="sticky">
-    <Toolbar>
-      <Typography variant="h6">
-       Guitar Traders
-       <div>
+return (
+  <nav className="nav">
+  <Link to="/" className="site-title">
+  LOGO Guitar Traders
+        </Link>
+        <ul>
+            <li>
             <Link to="/"> Shop </Link>
+            </li>
+            <li>
             <Link to="/guitars/cart">  Cart </Link>
+            </li>
+            <li>
             <Link to='/guitars/new'>  Sell </Link>
+            </li>
+            <li>
             <Link to="/guitars/contact">  Contact Us </Link>
+            </li>
+            <li>
             <Link to="/guitars/tuner">Tuner</Link>
-       </div>
-       <button onClick={handleDarkModeClick}>{buttonContent}</button>
-      </Typography>
-    </Toolbar>
-  </AppBar>
-</span>
-</>
-)
+            </li>
+            <button onClick={handleDarkModeClick}>{buttonContent}</button>
+        </ul>
+    </nav>
+  )
 }
 
 export default Nav
