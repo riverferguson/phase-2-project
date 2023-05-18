d9bd639bb63c3190f2bed1a9d2a780927e45e639
River Ferguson <riverferguson@gmail.com>
2023-05-18 14:44:50 -0700

    complete final css

--- phase-2-project/src/components/Search.js
+++ phase-2-project/src/components/Search.js
@@ -1,12 +1,8 @@
 import "./Search.css"
 
 
-const Search = ({ setSearchMake, setModelSearch}) => {
+const Search = ({setModelSearch}) => {
   
-
-  const handleMakeSearch = (event) => {
-    setSearchMake(event.target.value);
-  }
 
   const handleModelSearch = (event) => {
    setModelSearch(event.target.value)
@@ -18,13 +14,9 @@ const Search = ({ setSearchMake, setModelSearch}) => {
       <div className="search">
         <span>
           <label>Search by: Make or Model </label>
-          <label> </label>
-          {/* <input type='text' name="query" value='make' onChange={handleMakeSearch} />
-          <label>Make </label>
-          <input type='text' name="query"value='model' onChange={handleMakeSearch}/> */}
         </span>
         <span>
-          <input placeholder=" search "  onChange={handleModelSearch}/>
+          <input className="search-input" placeholder=" search "  onChange={handleModelSearch}/>
         </span>
       </div>
   )
