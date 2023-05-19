65bf8621bbd30fb5cbcea837304230de48c9011e
Jay Lim <jlimoman@gmail.com>
2023-05-19 01:50:39 -0600

    update footer style

--- phase-2-project/src/components/Footer.js
+++ phase-2-project/src/components/Footer.js
@@ -11,18 +11,18 @@ const Footer = () => {
         <>
           <div className='icon-bar' style={{ backgroundColor: '#c14d4d', color: 'white', padding: '25px' }}>
             <a href='https://twitter.com' target="_blank" rel='noreferrer'>
-            <TwitterIcon sx={{mr: 2}} />
+            <TwitterIcon sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
             </a>
             <a href='https://facebook.com' target="_blank" rel='noreferrer'>
-            <FacebookIcon sx={{mr: 2}} />
+            <FacebookIcon sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
             </a>
             <a href='https://instagram.com' target="_blank" rel='noreferrer'>
-            <InstagramIcon sx={{mr: 2}} />
+            <InstagramIcon sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
             </a>
             <a href='https://gmail.com/' target="_blank" rel='noreferrer'>
-            <MailOutlineIcon sx={{mr: 2}} />
+            <MailOutlineIcon sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
             </a>
-            Guitar Trader Inc. 
+            <p>Guitar Trader Inc &#8482;</p> 
             <a href='#root'>
             <button className="back-button" style={{marginLeft: '10px'}}> Back to Top</button>
             </a>
